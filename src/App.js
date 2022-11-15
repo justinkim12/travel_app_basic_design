6a57c8f66a48f7830a0f5f9ffa0c96001c0bf4e5
kim <justinkim1201@gmai.com>
2022-11-16 00:02:57 +0900

    travel_app_start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+import Bubble from "./routes/Bubble";
+import {useState,useEffect} from "react"
+import { BrowserView, MobileView } from 'react-device-detect'
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Switch>
+        {/* <MobileView> */}
+            <Route path='/movie/:id'> 
+            {/* path는 경로 컴포넌트로 사이트 띄우기 */}
+              <Detail/>
+            </Route>
+
+            <Route path='/bubble'>
+                <Bubble/>
+            </Route>
+            <Route path='/'>
+                <Home/>
+            </Route>
+
+        {/* </MobileView> */}
+        </Switch>
+    </Router>
   );
 }
-
 export default App;
